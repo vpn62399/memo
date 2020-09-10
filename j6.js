dba6cfde682f03a3f9f03d0ca2965c5f4f7f1ec5
kankanla <koyi_s@hotmail.com>
2020-09-10 12:30:15 +0900

    clt + shift + f

--- j6.js
+++ j6.js
@@ -7,170 +7,170 @@ var set1 = 0;  //0色の表示をつつける,1色一回のみ
 var set2 = 0;  //0色初期化する,1色初期化しない
 var set3 = 3;  //連番数
 
-function fns6 (x,set1=0){
+function fns6(x, set1 = 0) {
   //http://www.ohtashp.com/topics/takarakuji/index_loto6.html
   var url = 'http://www.ohtashp.com/topics/takarakuji/index_loto6.html'
-  if(location.href != url ){
+  if (location.href != url) {
     window.open(url);
     return;
   }
-  var colors = ['#FF6666','#99CC66','#33FFCC','#CC66CC','#FFFF00','#FF33CC','#FF6600','#99FF00','#CC66CC'];
+  var colors = ['#FF6666', '#99CC66', '#33FFCC', '#CC66CC', '#FFFF00', '#FF33CC', '#FF6600', '#99FF00', '#CC66CC'];
   var tb = document.getElementsByTagName('tbody')[0];
   var fc = tb.firstElementChild;
   var xca = 0;
-  var ns= document.getElementsByClassName('hon');
-  if (x[0] >= 10){
-      document.getElementById('c'+x[0]).click();
-  }else{
-      document.getElementById('c0'+x[0]).click();
+  var ns = document.getElementsByClassName('hon');
+  if (x[0] >= 10) {
+    document.getElementById('c' + x[0]).click();
+  } else {
+    document.getElementById('c0' + x[0]).click();
   }
-  do{
+  do {
     var trs = fc.getElementsByTagName('td');
-    if(trs.length < 3){
+    if (trs.length < 3) {
       break;
     }
-    var xc=0;
-    for(var i = 1; i< 7; i++){
-      for(var j = 0; j < x.length; j++){
-        if(trs[i].innerText == x[j]){
-          trs[i].style.backgroundColor=colors[j];
+    var xc = 0;
+    for (var i = 1; i < 7; i++) {
+      for (var j = 0; j < x.length; j++) {
+        if (trs[i].innerText == x[j]) {
+          trs[i].style.backgroundColor = colors[j];
           xc++;
-          if(set1 == 0){
-            x.splice(j,1);
+          if (set1 == 0) {
+            x.splice(j, 1);
           }
         }
       }
     }
-    
-    if(xc == 3 ){
-      trs[0].style.backgroundColor=colors[0];
+
+    if (xc == 3) {
+      trs[0].style.backgroundColor = colors[0];
       xca++;
     }
 
-    if(xc == 4){
-      trs[0].style.backgroundColor=colors[1];
+    if (xc == 4) {
+      trs[0].style.backgroundColor = colors[1];
       xca++;
     }
 
-    if(xc == 5){
-      trs[0].style.backgroundColor=colors[2];
+    if (xc == 5) {
+      trs[0].style.backgroundColor = colors[2];
       xca++;
     }
-  } while (fc=fc.nextElementSibling);
-  
-  if( xca > 4 ){
+  } while (fc = fc.nextElementSibling);
+
+  if (xca > 4) {
     console.log('fns6(  [' + x + ']  )' + "  d3c = " + xca);
   }
 }
 
-function ck(x){
-  if(location.href != 'http://www.ohtashp.com/topics/takarakuji/index_loto6.html'){
+function ck(x) {
+  if (location.href != 'http://www.ohtashp.com/topics/takarakuji/index_loto6.html') {
     alert("URL 不正 ファンクション不正");
     return;
   }
-  for(var i=0 ; i<x; i++){
+  for (var i = 0; i < x; i++) {
     var temp = l6(6);
-    if(x == 1){
+    if (x == 1) {
       console.log('fns6( [' + temp + '] )');
     }
     fns6(temp);
   }
 }
 
-function ad6kk (x){
+function ad6kk(x) {
   //http://www.ohtashp.com/topics/takarakuji/index_loto6.html
   var tb = document.getElementsByTagName('tbody');
   var pf = tb[0].firstElementChild;
   var cc = pf.cloneNode(true);
-  cc.getElementsByTagName('th')[0].innerText="ADD";
+  cc.getElementsByTagName('th')[0].innerText = "ADD";
   var tx = cc.getElementsByClassName('hon')
-  for(var i=0; i<6 ; i++){
+  for (var i = 0; i < 6; i++) {
     tx[i].innerText = x[i];
   }
-  tb[0].insertBefore(cc,pf)
+  tb[0].insertBefore(cc, pf)
 }
 
-function rd6 (x){
+function rd6(x) {
   //http://www.ohtashp.com/topics/takarakuji/index_loto6.html
   var tb = document.getElementsByTagName('tbody');
   var pf = tb[0].firstElementChild;
   var tx = pf.getElementsByClassName('hon')
-  for(var i=0; i<6 ; i++){
+  for (var i = 0; i < 6; i++) {
     tx[i].innerText = x[i];
   }
 }
 
-function rm6(){
+function rm6() {
   var tb = document.getElementsByTagName('tbody');
   var pf = tb[0].firstElementChild;
-  tb[0].removeChild(pf); 
+  tb[0].removeChild(pf);
 }
 
-function l6(x){
+function l6(x) {
   var j = [];
-  do{
+  do {
     var chk = "OK";
-    var c = Math.floor(Math.random()*(1-44)+44);
-    for (var i=0; i<j.length; i++){
-      if(j[i] == c){
+    var c = Math.floor(Math.random() * (1 - 44) + 44);
+    for (var i = 0; i < j.length; i++) {
+      if (j[i] == c) {
         chk = "NG";
       }
     }
-    if(chk != "NG"){
+    if (chk != "NG") {
       j.push(c);
     }
-  }while(j.length < x);
+  } while (j.length < x);
   return j.sort((a, b) => a - b);
 }
 
-function fik(x){
+function fik(x) {
   //指定の番号の列と一個前の列を表示
   //x=0の場合、すべての表示する。
   var tb = document.getElementsByTagName('tbody')[0];
-  var fc =tb.firstElementChild;
+  var fc = tb.firstElementChild;
   var pa = [];
   var nol = [];
-  var noli = new Array (44);
-  for(var i=1; i<44; i++){
+  var noli = new Array(44);
+  for (var i = 1; i < 44; i++) {
     noli[i] = 0;
   }
-  if(x==0){
-    do{
-      fc.style="";
-    }while(fc = fc.nextElementSibling);
+  if (x == 0) {
+    do {
+      fc.style = "";
+    } while (fc = fc.nextElementSibling);
     return null;
   }
-  do{
+  do {
     var temp = fc.getElementsByTagName('td');
-    if(temp.length < 11){
+    if (temp.length < 11) {
       break;
     }
     var temp2 = [];
-    for(var i=1; i<7;i++){
+    for (var i = 1; i < 7; i++) {
       temp2.push(parseInt(temp[i].innerText));
     }
-    if(temp2.indexOf(x) == -1){
-      fc.style.display= "none";
-    }else{
-      if(fc.previousElementSibling){
+    if (temp2.indexOf(x) == -1) {
+      fc.style.display = "none";
+    } else {
+      if (fc.previousElementSibling) {
         fc.previousElementSibling.style = " ";
-        var t3= fc.previousElementSibling.getElementsByTagName('td'); 
-        for(var i=1; i<7;i++){
+        var t3 = fc.previousElementSibling.getElementsByTagName('td');
+        for (var i = 1; i < 7; i++) {
           nol.push(parseInt(t3[i].innerText));
-          if(pa.indexOf(parseInt(t3[i].innerText)) == -1){
+          if (pa.indexOf(parseInt(t3[i].innerText)) == -1) {
             pa.push(parseInt(t3[i].innerText));
           }
         }
       }
     }
-  }while(fc = fc.nextElementSibling);
-  console.log('fns( [' + pa.sort((a,b) => a-b) + '] )' + '  x = ' + x );
+  } while (fc = fc.nextElementSibling);
+  console.log('fns( [' + pa.sort((a, b) => a - b) + '] )' + '  x = ' + x);
   fns6([x]);
-  nol=nol.sort((a,b)=>a-b)
-  do{
+  nol = nol.sort((a, b) => a - b)
+  do {
     var i = nol.pop();
-    noli[i] = noli[i] +1;
-  }while(nol.length !=0)
+    noli[i] = noli[i] + 1;
+  } while (nol.length != 0)
   console.table(noli);
   // noli.forEach(function(v,i,ar){
   //   console.log(" " + i + "   " + v);
@@ -190,11 +190,11 @@ function fik(x){
 var ms = [];
 var size = 0;
 var item2;
-var items ;
-var but ;
-var xx = function (){
+var items;
+var but;
+var xx = function () {
   var url = 'https://www.takarakuji-official.jp/ec/loto6/';
-  if(location.href != url){
+  if (location.href != url) {
     window.open(url);
     return;
   }
@@ -203,39 +203,39 @@ var xx = function (){
   but = item2[0].getElementsByClassName('m_btn m_lotteryNumInputFunc_btn m_btn__colorRandom m_btn__block');
 }();
 
-function gnum(){
-  ms[size]=[];
-  for( var i=0; i<items.length; i++){
-    ms[size].push(parseInt(items[i].innerText,10));
+function gnum() {
+  ms[size] = [];
+  for (var i = 0; i < items.length; i++) {
+    ms[size].push(parseInt(items[i].innerText, 10));
   }
-  size+=1;
+  size += 1;
   // console.log(ms);
 }
 
-function list(){
+function list() {
   var funlink = new String();
-  for(var i=0; i< ms.length; i++){
+  for (var i = 0; i < ms.length; i++) {
     funlink = i + '     fns6( [' + ms[i] + '] )';
     console.log(funlink);
   }
 }
 
-function fk(){
-   but[0].click()
-   gnum();
-   // list();
+function fk() {
+  but[0].click()
+  gnum();
+  // list();
 }
 
-function fkk(x){
-  var i =0;
-  var tid = setInterval(function (){
+function fkk(x) {
+  var i = 0;
+  var tid = setInterval(function () {
     fk();
     i++;
-    if(i>x){
+    if (i > x) {
       clearInterval(tid);
       list();
     }
-  },50);
+  }, 50);
 }
 
 //////////////////////////////////////////////////////////////////////////
--- j7.js
+++ j7.js
@@ -9,149 +9,149 @@ var set1 = 0;  //0色の表示をつつける,1色一回のみ
 var set2 = 0;  //0色初期化する,1色初期化しない
 var set3 = 3;  //連番数
 
-function ck1(){
-  set1=1;
+function ck1() {
+  set1 = 1;
   ck(1)
-  set1=0;
+  set1 = 0;
 }
 
-function rfns(x){
-  set1=1;
+function rfns(x) {
+  set1 = 1;
   rm7();
   fns(x);
-  set1=0;
+  set1 = 0;
 }
 
-var start = function(){
+var start = function () {
   var url = 'http://www.ohtashp.com/topics/takarakuji/'
-  if(location.href != url ){
+  if (location.href != url) {
     window.open(url);
   }
 }();
 
-function fns(x,set1=0,set2=0){
+function fns(x, set1 = 0, set2 = 0) {
   // xのアレーの数字を色で表示
   //http://www.ohtashp.com/topics/takarakuji/
   //https://www.mizuhobank.co.jp/retail/takarakuji/loto/loto7/index.html
-  var colors = ['#FF6666','#99CC66','#33FFCC','#CC66CC','#FFFF00','#FF33CC','#FF6600','#99FF00','#CC66CC','#ffd700','#ff00ff','#adff2f','#8a2be2','#808000','#ff1493','#2f4f4f','#40e0d0'];
-	var tb = document.getElementsByTagName('tbody')[1];
-	var fc = tb.firstElementChild;
-	var xca = 0;
-    if(set2 == 0){
-    	var ns= document.getElementsByClassName('hon');
-      for(var i = 0; i< ns.length; i++){
-        ns[i].style="";
-      }
-    var ns= document.getElementsByClassName('naka');
-    for(var i = 0; i< ns.length; i++){
-      ns[i].style="";
+  var colors = ['#FF6666', '#99CC66', '#33FFCC', '#CC66CC', '#FFFF00', '#FF33CC', '#FF6600', '#99FF00', '#CC66CC', '#ffd700', '#ff00ff', '#adff2f', '#8a2be2', '#808000', '#ff1493', '#2f4f4f', '#40e0d0'];
+  var tb = document.getElementsByTagName('tbody')[1];
+  var fc = tb.firstElementChild;
+  var xca = 0;
+  if (set2 == 0) {
+    var ns = document.getElementsByClassName('hon');
+    for (var i = 0; i < ns.length; i++) {
+      ns[i].style = "";
+    }
+    var ns = document.getElementsByClassName('naka');
+    for (var i = 0; i < ns.length; i++) {
+      ns[i].style = "";
     }
   }
 
-  do{
+  do {
     var trs = fc.getElementsByTagName('td');
-    var xc=0;
-    for(var i = 1; i< 8; i++){
-      for(var j = 0; j < x.length; j++){
-        if(trs[i].innerText == x[j]){
-          trs[i].style.backgroundColor=colors[j];
+    var xc = 0;
+    for (var i = 1; i < 8; i++) {
+      for (var j = 0; j < x.length; j++) {
+        if (trs[i].innerText == x[j]) {
+          trs[i].style.backgroundColor = colors[j];
           xc++;
-          if(set1 == 1){
-            x.splice(j,1);
+          if (set1 == 1) {
+            x.splice(j, 1);
           }
         }
       }
     }
-    
-    if(xc == 4 ){
-      trs[0].style.backgroundColor=colors[0];
+
+    if (xc == 4) {
+      trs[0].style.backgroundColor = colors[0];
       xca++;
     }
 
-    if(xc == 5){
-      trs[0].style.backgroundColor=colors[1];
+    if (xc == 5) {
+      trs[0].style.backgroundColor = colors[1];
       xca++;
     }
 
-    if(xc == 6){
-      trs[0].style.backgroundColor=colors[2];
+    if (xc == 6) {
+      trs[0].style.backgroundColor = colors[2];
       xca++;
     }
-  } while (fc=fc.nextElementSibling);
-	
-  if(xca > set3 ){
-    console.log('fns(  [' + x.sort((a,b) => a-b) + ']  )' + "  d4c = " + xca);
+  } while (fc = fc.nextElementSibling);
+
+  if (xca > set3) {
+    console.log('fns(  [' + x.sort((a, b) => a - b) + ']  )' + "  d4c = " + xca);
   }
 }
 
-function ck(k){
+function ck(k) {
   //k回のランダム7数字を出す
-  for(var i=0 ; i<k; i++){
-    var temp= l7(7);
-    if(k==1){
+  for (var i = 0; i < k; i++) {
+    var temp = l7(7);
+    if (k == 1) {
       console.log('fns( [' + temp + '] )');
       console.log(nom);
     }
-    fns(temp,1);
+    fns(temp, 1);
   }
 }
 
-function ad7 (x){
+function ad7(x) {
   //仮設番号の追加
   //http://www.ohtashp.com/topics/takarakuji/
   var tb = document.getElementsByTagName('tbody');
   var pf = tb[1].firstElementChild;
   var cc = pf.cloneNode(true);
-  cc.getElementsByTagName('th')[0].innerText="ADD";
+  cc.getElementsByTagName('th')[0].innerText = "ADD";
   var ff = cc.firstElementChild.nextElementSibling.nextElementSibling;
-  do{
-      ff.className="xxx";
-  }while(ff=ff.nextElementSibling)
+  do {
+    ff.className = "xxx";
+  } while (ff = ff.nextElementSibling)
   var ff = cc.firstElementChild.nextElementSibling;
-  for(var i=0; i<7; i++){
-    ff= ff.nextElementSibling;
-    ff.innerText=x[i];
+  for (var i = 0; i < 7; i++) {
+    ff = ff.nextElementSibling;
+    ff.innerText = x[i];
   }
-  tb[1].insertBefore(cc,pf)
+  tb[1].insertBefore(cc, pf)
 }
 
-function rd7 (x){
+function rd7(x) {
   //仮設番号の変更
   //http://www.ohtashp.com/topics/takarakuji/
   var tb = document.getElementsByTagName('tbody');
   var pf = tb[1].firstElementChild;
   var tx = pf.getElementsByClassName('xxx')
-  for(var i=0; i<7 ; i++){
+  for (var i = 0; i < 7; i++) {
     tx[i].innerText = x[i];
   }
 }
 
-function rm7(){
+function rm7() {
   var tb = document.getElementsByTagName('tbody');
   var pf = tb[1].firstElementChild;
-  tb[1].removeChild(pf); 
+  tb[1].removeChild(pf);
 }
 
-function l7(x){
+function l7(x) {
   // ランダムの数字を発生
   var j = [];
-  do{
+  do {
     var chk = "OK";
-    var c = Math.floor(Math.random()*(1-38)+38);
-    for (var i=0; i<j.length; i++){
-      if(j[i] == c){
+    var c = Math.floor(Math.random() * (1 - 38) + 38);
+    for (var i = 0; i < j.length; i++) {
+      if (j[i] == c) {
         chk = "NG";
       }
     }
-    if(chk != "NG"){
+    if (chk != "NG") {
       j.push(c);
     }
-  }while(j.length < x);
+  } while (j.length < x);
   return j.sort((a, b) => a - b);
 }
 
 var nom = [];  //出ないアレー
-function fnm(a,b){
+function fnm(a, b) {
   //aからb回目の再現未出数字を検索
   var xc = 0;
   var numx = [];
@@ -159,29 +159,29 @@ function fnm(a,b){
   var bnumx = [];
   var tb = document.getElementsByTagName('tbody')[1];
   var fc = tb.firstElementChild;
-  for(var j=1; j<38; j++){
+  for (var j = 1; j < 38; j++) {
     bnumx.push(j);
   }
-  do{
+  do {
     xc++;
-    if(xc >= a && xc < a+b){
+    if (xc >= a && xc < a + b) {
       var temp = fc.getElementsByTagName('td');
-      for(var i = 1; i < 8 ; i++ ){
-        if(numx.indexOf(parseInt(temp[i].innerText)) < 0){
+      for (var i = 1; i < 8; i++) {
+        if (numx.indexOf(parseInt(temp[i].innerText)) < 0) {
           numx.push(parseInt(temp[i].innerText));
-          numx.sort((a,b)=>a-b);
+          numx.sort((a, b) => a - b);
         }
       }
-    }else{
+    } else {
       break;
     }
-  }while(fc=fc.nextElementSibling);
-  if(numx.length == 37){
+  } while (fc = fc.nextElementSibling);
+  if (numx.length == 37) {
     console.log("37");
-  }else{
-    bnumx.filter(function(nx){
-      if(numx.indexOf(nx) == -1){
-        if(nom.indexOf(nx) == -1){
+  } else {
+    bnumx.filter(function (nx) {
+      if (numx.indexOf(nx) == -1) {
+        if (nom.indexOf(nx) == -1) {
           nom.push(nx)
         }
       }
@@ -191,51 +191,51 @@ function fnm(a,b){
   console.log('fns( [' + nom + '] )');
 }
 
-function fik(x){
+function fik(x) {
   //指定の番号の列と一個前の列を表示
   //x=0の場合、すべての表示する。
   var tb = document.getElementsByTagName('tbody')[1];
-  var fc =tb.firstElementChild;
+  var fc = tb.firstElementChild;
   var pa = [];
   var nol = [];
-  var noli = new Array (38);
-  for(var i=1; i<38; i++){
+  var noli = new Array(38);
+  for (var i = 1; i < 38; i++) {
     noli[i] = 0;
   }
-  if(x==0){
-    do{
-      fc.style="";
-    }while(fc = fc.nextElementSibling);
+  if (x == 0) {
+    do {
+      fc.style = "";
+    } while (fc = fc.nextElementSibling);
     return null;
   }
-  do{
+  do {
     var temp = fc.getElementsByTagName('td');
     var temp2 = [];
-    for(var i=1; i<8;i++){
+    for (var i = 1; i < 8; i++) {
       temp2.push(parseInt(temp[i].innerText));
     }
-    if(temp2.indexOf(x) == -1){
-      fc.style.display= "none";
-    }else{
-      if(fc.previousElementSibling){
+    if (temp2.indexOf(x) == -1) {
+      fc.style.display = "none";
+    } else {
+      if (fc.previousElementSibling) {
         fc.previousElementSibling.style = " ";
-        var t3= fc.previousElementSibling.getElementsByTagName('td'); 
-        for(var i=1; i<8;i++){
+        var t3 = fc.previousElementSibling.getElementsByTagName('td');
+        for (var i = 1; i < 8; i++) {
           nol.push(parseInt(t3[i].innerText));
-          if(pa.indexOf(parseInt(t3[i].innerText)) == -1){
+          if (pa.indexOf(parseInt(t3[i].innerText)) == -1) {
             pa.push(parseInt(t3[i].innerText));
           }
         }
       }
     }
-  }while(fc = fc.nextElementSibling);
-  console.log('fns( [' + pa.sort((a,b) => a-b) + '] )' + '  x = ' + x );
+  } while (fc = fc.nextElementSibling);
+  console.log('fns( [' + pa.sort((a, b) => a - b) + '] )' + '  x = ' + x);
   fns([x]);
-  nol=nol.sort((a,b)=>a-b)
-  do{
+  nol = nol.sort((a, b) => a - b)
+  do {
     var i = nol.pop();
-    noli[i] = noli[i] +1;
-  }while(nol.length !=0)
+    noli[i] = noli[i] + 1;
+  } while (nol.length != 0)
   console.table(noli);
   // noli.forEach(function(v,i,ar){
   //   console.log(" " + i + "   " + v);
@@ -255,11 +255,11 @@ function fik(x){
 var ms = [];
 var size = 0;
 var item2;
-var items ;
-var but ;
-var xx = function (){
+var items;
+var but;
+var xx = function () {
   var url = 'https://www.takarakuji-official.jp/ec/loto7/';
-  if(location.href != url ){
+  if (location.href != url) {
     window.open(url);
     return;
   }
@@ -268,39 +268,39 @@ var xx = function (){
   but = item2[0].getElementsByClassName('m_btn m_lotteryNumInputFunc_btn m_btn__colorRandom m_btn__block');
 }();
 
-function gnum(){
-  ms[size]=[];
-  for( var i=0; i<items.length; i++){
-    ms[size].push(parseInt(items[i].innerText,10));
+function gnum() {
+  ms[size] = [];
+  for (var i = 0; i < items.length; i++) {
+    ms[size].push(parseInt(items[i].innerText, 10));
   }
-  size+=1;
+  size += 1;
   // console.log(ms);
 }
 
-function list(){
+function list() {
   var funlink = new String();
-  for(var i=0; i< ms.length; i++){
+  for (var i = 0; i < ms.length; i++) {
     funlink = i + '     fns([' + ms[i] + '])';
     console.log(funlink);
   }
 }
 
-function fk(){
-   but[0].click()
-   gnum();
-   // list();
+function fk() {
+  but[0].click()
+  gnum();
+  // list();
 }
 
-function fkk(x){
-  var i =0;
-  var tid = setInterval(function (){
+function fkk(x) {
+  var i = 0;
+  var tid = setInterval(function () {
     fk();
     i++;
-    if(i>x){
+    if (i > x) {
       clearInterval(tid);
       list();
     }
-  },50);
+  }, 50);
 }
 
 //////////////////////////////////////////////////////////////////////////
