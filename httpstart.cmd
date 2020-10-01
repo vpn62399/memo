b196003e59df85a66ead55ff3a0f2f89eccd8a04
kankanla <koyi_s@hotmail.com>
2020-10-01 22:18:25 +0900

    l6

--- httpstart.cmd
+++ httpstart.cmd
@@ -1,4 +1,4 @@
 %HOMEPATH%\Desktop\python-3.7.9-embed-win32\python.exe -m http.server 80 --bind 192.168.137.1  --directory %HOMEPATH%\Desktop\memo
-%HOMEPATH%\Desktop\python-3.7.9-embed-win32\python.exe -m http.server 80 --bind 192.168.11.101 --directory %HOMEPATH%\Desktop\memo
+%HOMEPATH%\Desktop\python-3.7.9-embed-win32\python.exe -m http.server 80 --bind 192.168.11.102 --directory %HOMEPATH%\Desktop\memo
 %HOMEPATH%\Desktop\python-3.7.9-embed-win32\python.exe -m http.server 80 --bind 192.168.11.105 --directory %HOMEPATH%\Desktop\memo
 %HOMEPATH%\Desktop\python-3.7.9-embed-win32\python.exe -m http.server 80 --bind 192.168.1.221 --directory %HOMEPATH%\Desktop\memo
