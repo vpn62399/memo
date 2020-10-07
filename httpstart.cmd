734c7bcb010899e1a4e78786ceaa4aa2d5189a90
kankanla <koyi_s@hotmail.com>
2020-10-07 17:32:06 +0900

    cmd

--- httpstart.cmd
+++ httpstart.cmd
@@ -1,9 +1,8 @@
-SET PV=python-3.7.9-embed-win32
-SET PV=python-3.9.0-embed-win32
-echo PV;
+SET pver=python-3.7.9-embed-win32
+SET pver=python-3.9.0-embed-win32
 
-%HOMEPATH%\Desktop\%PV%\python.exe -m http.server 80 --bind 192.168.137.1  --directory %HOMEPATH%\Desktop\memo
-%HOMEPATH%\Desktop\%PV%\python.exe -m http.server 80 --bind 192.168.11.102 --directory %HOMEPATH%\Desktop\memo
-%HOMEPATH%\Desktop\%PV%\python.exe -m http.server 80 --bind 192.168.11.105 --directory %HOMEPATH%\Desktop\memo
-%HOMEPATH%\Desktop\%PV%\python.exe -m http.server 80 --bind 192.168.11.108 --directory %HOMEPATH%\Desktop\memo
-%HOMEPATH%\Desktop\%PV%\python.exe -m http.server 80 --bind 192.168.1.221 --directory %HOMEPATH%\Desktop\memo
+%HOMEPATH%\Desktop\%pver%\python.exe -m http.server 80 --bind 192.168.137.1  --directory %HOMEPATH%\Desktop\memo
+%HOMEPATH%\Desktop\%pver%\python.exe -m http.server 80 --bind 192.168.11.102 --directory %HOMEPATH%\Desktop\memo
+%HOMEPATH%\Desktop\%pver%\python.exe -m http.server 80 --bind 192.168.11.105 --directory %HOMEPATH%\Desktop\memo
+%HOMEPATH%\Desktop\%pver%\python.exe -m http.server 80 --bind 192.168.11.108 --directory %HOMEPATH%\Desktop\memo
+%HOMEPATH%\Desktop\%pver%\python.exe -m http.server 80 --bind 192.168.1.221 --directory %HOMEPATH%\Desktop\memo
