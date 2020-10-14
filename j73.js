da434dfb1226f72eea91f43b08aa6d2241d1b40c
kankanla <koyi_s@hotmail.com>
2020-10-14 10:45:46 +0900

    js

--- j73.js
+++ j73.js
@@ -1,6 +1,36 @@
 var db;
 var request = indexedDB.open("MyTestDatabase", 5);
 
+request.onerror = function () {
+    console.log('onerror')
+}
+
+request.onsuccess = function (event) {
+    console.log('onsuccess');
+    // db = event.target.result;
+}
+
+request.onupgradeneeded = function () {
+    console.log('onupgradeneeded');
+    // var objectStore = db.createObjectStore("name", { keyPath: "myKey" });
+}
+
+
+
+const customerData = [
+    { ssn: "444-44-4444", name: "Bill", age: 35, email: "bill@company.com" },
+    { ssn: "555-55-5555", name: "Donna", age: 32, email: "donna@home.org" }
+];
+
+
+
+
+
+
+
+
+
+
 request.onerror = function (event) {
     console.log('onerror')
     alert("Why didn't you allow my web app to use IndexedDB?!");
