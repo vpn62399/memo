278354700c9c5026a68ded583e8a0eac4e7936ff
kankanla <koyi_s@hotmail.com>
2020-10-23 02:21:58 +0900

    hs

--- httpstart.cmd
+++ httpstart.cmd
@@ -7,3 +7,5 @@ SET pver=python-3.9.0-embed-win32
 %HOMEPATH%\Desktop\%pver%\python.exe -m http.server 80 --bind 192.168.11.102 --directory %HOMEPATH%\Desktop\memo
 %HOMEPATH%\Desktop\%pver%\python.exe -m http.server 80 --bind 192.168.11.105 --directory %HOMEPATH%\Desktop\memo
 %HOMEPATH%\Desktop\%pver%\python.exe -m http.server 80 --bind 192.168.11.108 --directory %HOMEPATH%\Desktop\memo
+
+%HOMEPATH%\Desktop\%pver%\python.exe -m http.server 80 --bind 127.0.0.1 --directory %HOMEPATH%\Desktop\memo
