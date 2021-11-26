742b86137c31a92fd922e181f126faea1c0aa966
unknown <koyi_s@hotmail.com>
2021-11-27 04:48:28 +0900

    -map_metadata 0

--- ffto.cmd
+++ ffto.cmd
@@ -1,13 +1,13 @@
 echo off
 echo rem FFmpeg > filelist.cmd
 md H264_qsv
-for %%a in (*.mp4) do echo ffmpeg -i %%a -r 29.97 -c:v h264_qsv -b:v 12M .\H264_qsv\%%a >> filelist.cmd
-for %%a in (*.MTS) do echo ffmpeg -i %%a -r 29.97 -c:v h264_qsv -b:v 12M -vf bwdif .\H264_qsv\%%a.mp4 >> filelist.cmd
-for %%a in (*.avi) do echo ffmpeg -i %%a -r 29.97 -c:v h264_qsv -b:v 9M -vf bwdif .\H264_qsv\%%a.mp4 >> filelist.cmd
+for %%a in (*.mp4) do echo ffmpeg -i %%a -map_metadata 0 -r 29.97 -c:v h264_qsv -b:v 12M .\H264_qsv\%%a >> filelist.cmd
+for %%a in (*.MTS) do echo ffmpeg -i %%a -map_metadata 0 -r 29.97 -c:v h264_qsv -b:v 12M -vf bwdif .\H264_qsv\%%a.mp4 >> filelist.cmd
+for %%a in (*.avi) do echo ffmpeg -i %%a -map_metadata 0 -r 29.97 -c:v h264_qsv -b:v 9M -vf bwdif .\H264_qsv\%%a.mp4 >> filelist.cmd
 
 rem md H264
-rem for %%a in (*.mp4) do echo ffmpeg -i %%a -r 29.97 -c:v libx264 -crf 23 .\H264\%%a >> filelist.cmd
-rem for %%a in (*.MTS) do echo ffmpeg -i %%a -r 29.97 -c:v libx264 -crf 23 -vf bwdif .\H264\%%a.mp4 >> filelist.cmd
+rem for %%a in (*.mp4) do echo ffmpeg -i %%a -map_metadata 0 -r 29.97 -c:v libx264 -crf 23 .\H264\%%a >> filelist.cmd
+rem for %%a in (*.MTS) do echo ffmpeg -i %%a -map_metadata 0 -r 29.97 -c:v libx264 -crf 23 -vf bwdif .\H264\%%a.mp4 >> filelist.cmd
 
 rem md MP3
 rem for %%a in (*.mp4) do echo ffmpeg -i %%a .\MP3\%%a.mp3 >> filelist.cmd
@@ -25,8 +25,8 @@ rem pause
 
 
 rem sample
-rem for %%a in (*.avi) do echo "ffmpeg -i %%a -vf vidstabdetect -an -f null - && ffmpeg -i %%a -vf vidstabtransform -r 29.97 -c:v h264_qsv -b:v 9M -vf bwdif .\H264_qsv\%%a.mp4" >> filelist.cmd
-rem for %%a in (*.avi) do echo ffmpeg -i %%a -r 29.97 -c:v h264_qsv -b:v 9M -vf bwdif -movflags +faststart .\H264_qsv\%%a.mp4 >> filelist.cmd
+rem for %%a in (*.avi) do echo "ffmpeg -i %%a -map_metadata 0 -vf vidstabdetect -an -f null - && ffmpeg -i %%a -vf vidstabtransform -r 29.97 -c:v h264_qsv -b:v 9M -vf bwdif .\H264_qsv\%%a.mp4" >> filelist.cmd
+rem for %%a in (*.avi) do echo ffmpeg -i %%a -map_metadata 0 -r 29.97 -c:v h264_qsv -b:v 9M -vf bwdif -movflags +faststart .\H264_qsv\%%a.mp4 >> filelist.cmd
 
 
 
