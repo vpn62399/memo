26a476df2b0182f3f46c47daeb3b5baf4f9143fb
unknown <koyi_s@hotmail.com>
2021-12-16 01:44:31 +0900

    RTX1100

---
+++ RTX1100_GET_Config_CMD.cmd
@@ -0,0 +1,2 @@
+tftp 192.168.0.1 get config/password RTX1100_config.txt
+tftp 192.168.0.1 get techinfo/password RTX1100_techinfo.txt
---
+++ RTX1100_PUT_Config _CMD.cmd
@@ -0,0 +1,2 @@
+rem tftp 192.168.0.1 put  RTX1100_config.txt config/password
+pause
