262462199dd986cca7d499067954366208f14dc8
unknown <koyi_s@hotmail.com>
2023-03-25 09:47:02 +0900

    py7

--- Python_loto7.py
+++ Python_loto7.py
@@ -140,22 +140,22 @@ def bas2(tag, l=9):
 # bas2([7,11])
 
 
-def t1():
-    url1 = 'https://raw.githubusercontent.com/kankanla/memo/master/loto7.csv'
-    url2 = 'https://raw.githubusercontent.com/kankanla/memo/master/loto7c.csv'
-    # lpd = pd.read_csv(url1, header=None)
-    # lpd = pd.read_csv(url1,names=('t1','d1','s1','s2','s3','s4','s5','s6','s7','b1','b2','n1','n2','n3','z1','z2'))
-    # lpd = pd.read_csv(url1,names=('t1','d1','s1','s2','s3','s4','s5','s6','s7','b1','b2','n1','n2','n3','z1','z2'))
-    lpd = pd.read_csv(url1, names=('t1', 'd1', 's1', 's2', 's3', 's4', 's5', 's6', 's7',
-                      'b1', 'b2', 'n1', 'n2', 'n3', 'z1', 'z2'), usecols=[1, 2, 3, 4, 5, 6, 7, 8, ])
-    # lpd = pd.read_csv(url1, header=None, usecols=[1, 2, 3, 4, 5, 6, 7, 8, ])
+# def t1():
+    # url1 = 'https://raw.githubusercontent.com/kankanla/memo/master/loto7.csv'
+    # url2 = 'https://raw.githubusercontent.com/kankanla/memo/master/loto7c.csv'
+    # # lpd = pd.read_csv(url1, header=None)
+    # # lpd = pd.read_csv(url1,names=('t1','d1','s1','s2','s3','s4','s5','s6','s7','b1','b2','n1','n2','n3','z1','z2'))
+    # # lpd = pd.read_csv(url1,names=('t1','d1','s1','s2','s3','s4','s5','s6','s7','b1','b2','n1','n2','n3','z1','z2'))
+    # lpd = pd.read_csv(url1, names=('t1', 'd1', 's1', 's2', 's3', 's4', 's5', 's6', 's7',
+    #                   'b1', 'b2', 'n1', 'n2', 'n3', 'z1', 'z2'), usecols=[1, 2, 3, 4, 5, 6, 7, 8, ])
+    # # lpd = pd.read_csv(url1, header=None, usecols=[1, 2, 3, 4, 5, 6, 7, 8, ])
 
-    lpd.set_index('d1')
-    lpd.index
-    lpd.plot()
-    # lpd.plot('d1', ['s1', 's2', 's3', 's4', 's5', 's6', 's7'])
-    plt.show()
-    print('77')
+    # lpd.set_index('d1')
+    # lpd.index
+    # lpd.plot()
+    # # lpd.plot('d1', ['s1', 's2', 's3', 's4', 's5', 's6', 's7'])
+    # plt.show()
+    # print('77')
 
 
 def tx1(num):
